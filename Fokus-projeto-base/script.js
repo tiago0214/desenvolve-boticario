9bb0d9541880730a8620c5ceb06e260ab5989726
Tiago Souza <tiago0214@gmail.com>
2024-04-03 15:22:26 +0100

    att

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -4,20 +4,51 @@ const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const starPauseBt = document.querySelector('#start-pause');
+const iniciarOuPausarBt = document.querySelector('#start-pause span');
+const imagemBt = document.querySelector('.app__card-primary-butto-icon');
+const tempoNaTela = document.querySelector('#timer');
+
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+const play = new Audio('/sons/play.wav');
+const pause = new Audio('/sons/pause.mp3');
+const beep = new Audio('/sons/beep.mp3');
+musica.loop = true;
+
+let contadorRegressivoMinutos = 1500;
+let intervaloId = null;
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+})
 
 focuBt.addEventListener('click', () => {
     alteraContexto('foco');
+    focuBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
+    contadorRegressivoMinutos = 300;
     alteraContexto('descanso-curto');
+    curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
+    contadorRegressivoMinutos = 900;
     alteraContexto('descanso-longo');
+    longoBt.classList.add('active');
 })
 
 function alteraContexto(contexto) {
+    mostrarTempo()
+    botoes.forEach((element) => element.classList.remove('active'));
+
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
 
@@ -36,10 +67,50 @@ function alteraContexto(contexto) {
             break;
         case "descanso-longo":
             titulo.innerHTML = `
-            Hora de voltar à superficies,<br>
+            Hora de voltar à superficie,<br>
                 <strong class="app__title-strong">faça uma pausa longa.</strong>
             `;
         default:
             break;
     }
 }
+
+const decrementarContador = () => {
+    if (contadorRegressivoMinutos === 0) {
+        beep.play();
+        zerar();
+        alert('Contagem terminou');
+        return;
+    }
+    contadorRegressivoMinutos--;
+    mostrarTempo();
+
+}
+
+starPauseBt.addEventListener('click', iniciarOuPausar);
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        pause.play();
+        zerar();
+        return;
+    }
+    imagemBt.setAttribute('src', '/imagens/pause.png');
+    iniciarOuPausarBt.textContent = 'Pausar';
+    play.play();
+    intervaloId = setInterval(decrementarContador, 1000);
+}
+
+function zerar() {
+    imagemBt.setAttribute('src', '/imagens/play_arrow.png');
+    iniciarOuPausarBt.textContent = 'Continuar';
+    clearInterval(intervaloId);
+    intervaloId = null;
+}
+
+function mostrarTempo() {
+    let tempo = new Date(contadorRegressivoMinutos * 1000);
+    let tempoFormatado = tempo.toLocaleTimeString('pt-Br', { minute: '2-digit', second: '2-digit' });
+    tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+mostrarTempo();
