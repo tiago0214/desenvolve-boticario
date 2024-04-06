e29e456363421c1d5ae097696ea6ad9da3daf537
Tiago Souza <tiago0214@gmail.com>
2024-04-06 08:10:41 +0100

    att

--- aluramidi-projeto/main.js
+++ aluramidi-projeto/main.js
@@ -2,6 +2,34 @@ const listaSons = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
+while (contador < listaSons.length) {
+    const tecla = listaSons[contador];
+    const intrumento = tecla.classList[1];
+    const som = `#som_${intrumento}`;
 
+    tecla.onclick = function () {
+        tocarSom(som);
+    };
 
-listaSons[1].onclick = document.querySelector('#som_tecla_pom').play();
+    tecla.onkeydown = function (event) {
+        if (event.key == 'Enter' || event.key == 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+    contador++
+}
+
+function tocarSom(id) {
+    const elemento = document.querySelector(id);
+
+    if (elemento && elemento.localName == 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado');
+    }
+}
