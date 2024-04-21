4f9d6569172ee8b9c2a52ba09ec06ed508fb25e7
Tiago Souza <tiago0214@gmail.com>
2024-04-21 23:01:18 +0100

    att

---
+++ bank/js/valida-idade.js
@@ -0,0 +1,11 @@
+export default function ehMaiorDeIdade(campo) {
+    const dataDeNascimento = new Date(campo.value);
+    validaIdade(dataDeNascimento);
+}
+
+function validaIdade(data) {
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataAtual >= dataMais18;
+};
